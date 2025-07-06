ae659fadd93cc2984d17c2e8907e6779ad17a6e0
chris <chris@buildlackey.com>
2025-07-05 17:00:07 -0700

    removed map api key from html

--- ui/Code.js
+++ ui/Code.js
@@ -1,6 +1,6 @@
 function onOpen() {
   SpreadsheetApp.getUi()
-    .createMenu('📍 Distance Filter')
+    .createMenu('📍 Distance Filter2')
     .addItem('Filter By Distance', 'showAutocompleteDialog')
     .addItem('Add Lat/Long Info', 'populateLatLong')
     .addToUi();
@@ -10,6 +10,13 @@ function showAutocompleteDialog() {
   const html = HtmlService.createHtmlOutputFromFile('FilterUI')
     .setWidth(700)   // ✅ Now applies in modal dialogs
     .setHeight(500); // Optional, but helps avoid scroll
-  SpreadsheetApp.getUi().showModalDialog(html, 'Address Autocomplete');
+  SpreadsheetApp.getUi().showModalDialog(html, 'Filter by Distance');
+}
+
+
+function getMapsApiKey() {
+  const key = PropertiesService.getScriptProperties().getProperty("GOOGLE_MAPS_API_KEY");
+  if (!key) throw new Error("API key not found in script properties.");
+  return key;
 }
 
