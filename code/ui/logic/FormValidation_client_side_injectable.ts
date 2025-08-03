8e8abd4f57623561f5e8e6ac1118776deefe19b6
chris <chris@buildlackey.com>
2025-08-03 16:37:04 -0700

    put in proper place: convert_to_client_side_injectable.sh

--- code/ui/logic/FormValidation_client_side_injectable.ts
+++ code/ui/logic/FormValidation_client_side_injectable.ts
@@ -1,7 +1,7 @@
 const FormValidation_client_side_injectable = {
   MESSAGE: "Injected via include",
   hello() : string {
-      return "👋 fat roo from FormValidation_client_side_injectable.hello()";
+      return "👋 cat doo china roo from FormValidation_client_side_injectable.hello()";
   },
   validateForm(data: any) {
     return { valid: false, errors: ["Still a placeholder"] };
