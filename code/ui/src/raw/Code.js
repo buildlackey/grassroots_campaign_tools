6c6c057074935277a19b843dbfd48152795d3534
chris <chris@buildlackey.com>
2025-08-11 15:48:18 -0700

    adjust tool tips. works for pos. but not clickable links

--- code/ui/src/raw/Code.js
+++ code/ui/src/raw/Code.js
@@ -12,7 +12,7 @@ function include(filename) {
 /** Add a minimal menu to open the Settings dialog */
 function onOpen() {
   SpreadsheetApp.getUi()
-    .createMenu('⚙️ Current Settings')
+    .createMenu('⚙️ The Settings')
     .addItem('Open Settings', 'showSettingsDialog')
     .addToUi();
 }
