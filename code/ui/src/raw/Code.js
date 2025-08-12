5e31c88bb69bd125ed46473fe28d601ada140037
chris <chris@buildlackey.com>
2025-08-11 18:52:15 -0700

    nicer buttons.  chmod a+x on all scripts.

--- code/ui/src/raw/Code.js
+++ code/ui/src/raw/Code.js
@@ -12,7 +12,7 @@ function include(filename) {
 /** Add a minimal menu to open the Settings dialog */
 function onOpen() {
   SpreadsheetApp.getUi()
-    .createMenu('⚙️ The Settings')
+    .createMenu('⚙️ Settings Dialog mouse')
     .addItem('Open Settings', 'showSettingsDialog')
     .addToUi();
 }
